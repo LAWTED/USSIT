be014b351462ef064cfe59a9c44d9d263a22500c
LAWTED <3209571656@qq.com>
2022-01-05 23:47:46 +0800

    finishi chart2

---
+++ src/components/echart/IPG/chart2/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <Chart :cdata="cdata" />
+  </div>
+</template>
+
+<script>
+import Chart from './chart.vue'
+export default {
+  props: {
+    block_data: Array
+  },
+  data() {
+    return {
+      cdata: {
+        block_data: this.block_data,
+        category: ['Mon', 'Tue', 'Wen', 'Thr', 'Fri', 'Sat', 'Sun']
+      }
+    };
+  },
+  components: {
+    Chart,
+  },
+  mounted() {
+    // this.setData();
+  },
+  methods: {
+    // 根据自己的业务情况修改
+    // setData () {
+    //   for (let i = 0; i < this.cdata.barData.length -1; i++) {
+    //     let rate = this.cdata.barData[i] / this.cdata.lineData[i];
+    //     this.cdata.rateData.push(rate.toFixed(2));
+    //   }
+    // },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
